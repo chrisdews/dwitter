7a0d1ed8d260922d23ee55496f6b2106dd224f3c
chrisdews <cldews1@gmail.com>
2019-06-09 17:57:50 +0100

    controllers added for Pages, routes/views set up for index, profile, explore

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :pages, only: [:index, :profile, :explore]
+  get 'pages/index'
+  get 'pages/profile'
+  get 'pages/explore'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
